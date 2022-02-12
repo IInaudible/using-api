ec48df09dcd554ad833adeaa20a3bce82020ad02
halowe-cfa <91778081+halowe-cfa@users.noreply.github.com>
2022-02-12 12:02:50 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+//create an asynchronous function called start
+//create a const variable called response and set it equal to await fetch(url)
+//create a const variable called data and set it equal to await response.json()
+//what data are you receiving back? console.log(data)
+//add function for Breedlist
+//Call start function
+
+
+
+//create creatBreedList function
+//pass in breedList as parameter
+//use getElementById to get ("breed") innerHTML
+//use template literals to create dog breed drop down
+//use Object class and keys methos to map breed
+//add .join to make a list
+
+
+
+//add async function that responds to dog breed 
+//check with alert
+//set const response equal to await fetch with api
+//set const data equal to await response.json()
+//console.log(data)
